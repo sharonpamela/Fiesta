58481b1740357a99fc3100a93017347bfa327495
sharonpamela <sharon.pamela.santana@gmail.com>
2020-01-29 10:59:52 -0800

    changed config file to add prod group

--- config/config.js
+++ config/config.js
@@ -6,5 +6,13 @@ module.exports = {
       "host": "localhost",
       "port": "3306",
       "dialect": "mysql"
-    }
+    },
+    "production": {
+        "database": "FiestaDB",
+        "username": "root",
+        "password": "nutanix/4u",
+        "host": "localhost",
+        "port": "3306",
+        "dialect": "mysql"
+      }
 }
